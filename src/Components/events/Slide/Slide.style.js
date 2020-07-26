b19a374805ab055a24f5dd5bef16334a37b4ed47
Karno Sose <karnosose@gmail.com>
2020-07-26 14:53:39 +0400

    added slide and location components for events

---
+++ src/Components/events/Slide/Slide.style.js
@@ -0,0 +1,8 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles({
+    sliderContainer: {
+        width: 500,
+        marginBottom: 40,
+    }
+})
--- src/customs.js
+++ src/customs.js
@@ -134,6 +134,7 @@ export const event = [
     },
     {
         type: 'SLIDE',
+        sliderType: '', //expect 'cube' or 'fade'
         src: ['https://as00.epimg.net/img/les/20200229/dea084f2530b36689bdb17e63b4f198f/69d914a4ff7cb78c31abd19e73eba0ca_720.png',
                 'https://as01.epimg.net/en/imagenes/2020/02/28/football/1582900958_522512_1582901120_noticia_normal_recorte1.jpg',
                 'https://en.as.com/en/imagenes/2020/02/29/football/1582969808_951576_1582969867_noticia_normal_recorte1.jpg'
