9e8d33569fbb247fdd6a23edb286f42dcf912b6f
Karno Sose <karnosose@gmail.com>
2020-07-26 16:44:21 +0400

    redesigning slide

--- src/view/Events/Events.js
+++ src/view/Events/Events.js
@@ -2,12 +2,14 @@ import React from "react";
 import {event} from '../../customs';
 import renderComponent from './renderComponent';
 
+
+
 function Events(props) {
     const _event = event;
 
     console.log(_event)
     return (
-        <div className={'page '}>
+        <div className={'page'}>
             {_event.map(renderComponent)}
         </div>
     );
